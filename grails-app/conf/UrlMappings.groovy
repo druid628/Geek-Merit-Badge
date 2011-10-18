3f127bcaa514360f25cf1daf271d6b1c6127fd1a
Micah Breedlove <micah.breedlove@iostudio.com>
2011-10-18 15:27:14 -0500

    adding support for / to view user

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,17 @@
 class UrlMappings {
 
 	static mappings = {
+		"/$username" {
+			controller = "person"
+			action = "view"
+		}
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
+
 
 		"/"(view:"/index")
 		"500"(view:'/error')
--- grails-app/controllers/com/druid628/gmb/PersonController.groovy
+++ grails-app/controllers/com/druid628/gmb/PersonController.groovy
@@ -3,5 +3,10 @@ package com.druid628.gmb
 class PersonController {
 
 	//    def index = { }
+	def view =  {
+		def usr = new Person(username: params.username)
+		def usersBadges = new Person2Badge(user: usr.id)
+	}
+
 	def scaffold = Person
 }
